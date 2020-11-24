f3ae800aac786c9db2da89ac1ab9f7a0d802d875
Jaehun Heo <jhheo@aegisdds.com>
2020-11-24 18:27:20 +0900

    인터럽트, Edge counter 예제 추가

---
+++ TI/Tiva/Interrupt_EdgeCounter/main.c
@@ -0,0 +1,42 @@
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+
+extern void EnableInterrupts();
+extern void WaitForInterrupt();
+
+volatile uint32_t FallingEdges = 0;
+
+void EdgeCounter_Init(void){
+  SYSCTL_RCGCGPIO_R |= 0x00000020; // (a) activate clock for port F
+  while((SYSCTL_PRGPIO_R & 0x20) != 0x20);
+  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
+  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4
+  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
+  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
+  EnableInterrupts();           // (i) Enable global Interrupt flag (I)
+}
+
+void GPIOPortF_Handler(void){
+  GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
+  FallingEdges = FallingEdges + 1;
+}
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    EdgeCounter_Init();
+
+    while(1)
+    {
+        WaitForInterrupt();
+    }
+	return 0;
+}
